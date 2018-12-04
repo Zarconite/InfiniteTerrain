12fee366cb8f683460c65c740c6e8f62b3719fc9
Zarconite <zarconite@gmail.com>
2018-12-04 14:22:41 +0000

    Infinite terrain, Gps now works and shows the grid, north south east west all work and encounters are persistant. To add onto this, every blank square now has one of ten random terrains which are also persistant. There's also a setting that lets you see where you've been before. The map is still 500x500, but will be expanded in the 4d array soon(tm)

--- src/infiniteTerrain/Main.java
+++ src/infiniteTerrain/Main.java
@@ -10,24 +10,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//For loop to show grid position +5 spaces and -5 spaces in any direction? ie: 11x11 grid
-		
 		Game.addEncounters();
 		
 		int xPosition = Map.size/2;
 		int yPosition = Map.size/2;
+		boolean exploredTilesTrue = false;
 		
 		/*for(Encounter encountersPrint:Encounter.encountersList) { //temporary to ensure all encounters print via the overridden toString() method in Encounter
 			System.out.println(encountersPrint.toString());
 		}*/
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("<Welcome message telling you to type 'look'>");
+		System.out.println("Do you want the map to show where you've travelled to? (Yes/No)");
 		String inputCommand = scanner.nextLine();
+		if(inputCommand.toLowerCase().equals("yes")) {
+			exploredTilesTrue=true;
+		}
+		
+		System.out.println("<Welcome message telling you to type 'look'>");
+		inputCommand = scanner.nextLine();
+		
 		if(inputCommand.toLowerCase().equals("look")) {
 			System.out.println("You have a GPS and can use 'north', 'south', 'east', 'west' to move. You can type 'use' to check the location of the nearest encounter");
 		}
-		//Map.addTheEncounters();
+		
 		infiniteTerrain.Map gameMap = new Map();
 		gameMap.populateSquares((Map.size*Map.size)/8); //12.5% chance of encounter generating in any tile
 		
@@ -37,25 +43,25 @@ public class Main {
 		if(inputCommand.toLowerCase().equals("north")) {
 			yPosition--;
 			System.out.println(currentLocation(xPosition,yPosition));
-			System.out.println(SquareText(xPosition,yPosition));
+			System.out.println(SquareText(xPosition,yPosition,exploredTilesTrue));
 		}
 		
 		if(inputCommand.toLowerCase().equals("east")) {
 			xPosition++;
 			System.out.println(currentLocation(xPosition,yPosition));
-			System.out.println(SquareText(xPosition,yPosition));
+			System.out.println(SquareText(xPosition,yPosition,exploredTilesTrue));
 		}
 		
 		if(inputCommand.toLowerCase().equals("south")) {
 			yPosition++;
 			System.out.println(currentLocation(xPosition,yPosition));
-			System.out.println(SquareText(xPosition,yPosition));
+			System.out.println(SquareText(xPosition,yPosition,exploredTilesTrue));
 		}
 		
 		if(inputCommand.toLowerCase().equals("west")) {
 			xPosition--;
 			System.out.println(currentLocation(xPosition,yPosition));
-			System.out.println(SquareText(xPosition,yPosition));
+			System.out.println(SquareText(xPosition,yPosition,exploredTilesTrue));
 		}
 		
 		if(inputCommand.toLowerCase().equals("use")) {
@@ -68,27 +74,32 @@ public class Main {
 	}
 	
 	public static String currentLocation(int xPosition, int yPosition) {
-		return "You are currently at x: "+xPosition+" y: "+yPosition;
+		return "You are currently at x: "+xPosition+" y: "+(Map.size-yPosition);
 	}
 	
 	public static void use(int xPosition, int yPosition) {
-		System.out.println("You check your GPS: ");
+		System.out.println("You check your GPS, you are currently at x: "+xPosition+" y: "+(Map.size-yPosition)+":");
+		
 		for(int j=-3; j<=3; j++) {
 			for(int k=-3; k<=3; k++) {
-				if(j==0&&k==0) {System.out.print("[X]");}
-				else if(Map.Map[0][0][k+xPosition][j+yPosition].charAt(0)==' ') { //implement -5 and the current position
+				
+				if(j==0&&k==0) {
+					System.out.print("[X]");
+					}
+				else if(Map.Map[0][0][k+xPosition][j+yPosition].charAt(0)==' ') { 
 					System.out.print("[ ]");
 				}
 				else {
 					System.out.print("["+Map.Map[0][0][k+xPosition][j+yPosition]+"]");
 				}
+				
 			}
 			System.out.println();
 		}
 		
 	}
 	
-	public static String SquareText(int xPosition, int yPosition) {
+	public static String SquareText(int xPosition, int yPosition, boolean exploredTilesTrue) {
 		String[] noEncounter = {
 				"Current terrain: Light Woodland",
 				"Current terrain: Light Woodland",
@@ -102,11 +113,27 @@ public class Main {
 				"Current terrain: Dead woodland",
 				"Current terrain: Abandoned buildings"
 				};
+		String returner="";
 		if(Map.Map[0][0][xPosition][yPosition].charAt(0)==' ') {
-			return noEncounter[Integer.parseInt(Map.Map[0][0][xPosition][yPosition].replaceAll("\\s+",""))];
+			returner = noEncounter[Integer.parseInt(Map.Map[0][0][xPosition][yPosition].replaceAll("\\s+",""))];
+			setExplored(xPosition, yPosition,exploredTilesTrue);
+			return returner;
+		}
+		else if(Map.Map[0][0][xPosition][yPosition] == "+") {
+			return "You've been here before";
 		}
 		else {
-		return String.valueOf(Encounter.encountersList.get(Integer.parseInt(Map.Map[0][0][xPosition][yPosition])));
+		returner = String.valueOf(Encounter.encountersList.get(Integer.parseInt(Map.Map[0][0][xPosition][yPosition])));
+		setExplored(xPosition, yPosition,exploredTilesTrue);
+		return returner;
+		}
+	}
+	public static void setExplored(int xPosition, int yPosition,boolean exploredTilesTrue) { 
+		if(Map.Map[0][0][xPosition][yPosition].charAt(0)!=' ' && exploredTilesTrue==false) {
+		Map.Map[0][0][xPosition][yPosition] = " "+Map.Map[0][0][xPosition][yPosition];
+		}
+		else if(exploredTilesTrue){
+			Map.Map[0][0][xPosition][yPosition] = "+";
 		}
 	}
 
